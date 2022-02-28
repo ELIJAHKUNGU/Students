74bcd40b5567c0b411ac6efc8944ffc422f77126
ELIJAHKUNGU <elijahkungu100@gmail>
2022-02-28 09:22:19 +0300

    backend

--- forest/login.php
+++ forest/login.php
@@ -1,90 +1,64 @@
-<?php
-
-
-if (isset($_POST["password"])) {
-    require 'DB.php';
-    extract($_POST);
-    $sql = "select * from users where email='$email' and password='$password' LIMIT 1";
-
-    $result= mysqli_query($conn, $sql);
-
-    if (mysqli_num_rows($result) ==1){
-        //success
-        $info = mysqli_fetch_assoc($result);
-        session_start();
-        $_SESSION["info"] = $info;
-        header("location:index.php");
-
-    }else{
-        $message="Wrong username or password";
-    }
-
-}
-?>
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sign In</title>
     <link rel="stylesheet" href="css/bootstrap.css">
 </head>
+
 <body>
-<?php
-//include 'navbar.php';
-?>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-sm-6">
-            <div class="card">
-                <div class="card-header bg-success">
-                    <h2 class="text-white text-center font-weight-bold" style="font-size: 25px">
-                        Sign in
-                    </h2>
+
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-sm-6 pt-5 mt-5">
+                <div class="shadow">
+                    <div class=" ">
+                        <h2 class=" text-center pt-5 font-weight-bold" style="font-size: 25px">
+                            Sign in
+                        </h2>
+                    </div>
+                    <div class="card-body">
+                        <form action="login.php" method="post">
+
+                            <div class="form-group">
+                                <label for="title">Email</label>
+                                <input type="email" class="form-control pt-4 pb-4" name="email" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="title">Password</label>
+                                <input type="password" class="form-control pt-4 pb-4" name="password" required>
+                            </div>
+
+                            <div class="d-flex">
+                                <button style="text-transform: uppercase;" class="btn btn-success mr-3 ">Sign in</button>
+
+
+                        </form>
+                        <a style="text-transform: uppercase;" href="./register.html">Register</a>
+                        </div>
+
+
+                    </div>
                 </div>
-                 <div class="card-body">
-
-
-                        <?php
-                        if (isset($message))
-                            echo "<p class='text-danger'>$message</p>";
-                        ?>
-
-                     <form action="login.php" method="post" >
-
-                        <div class="form-group">
-                            <label for="title">Email</label>
-                            <input type="email" class="form-control" name="email" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="title">Password</label>
-                            <input type="password" class="form-control" name="password" required>
-                        </div>
-
-                       <div class="d-flex">
-                       <button class="btn btn-info btn-block mr-3 col-sm-2">Sign in</button>
-                       </div>
-
-                        
-
-                     </form>
-                     <a href="./register.php"><button class="btn btn-info mt-3 col-sm-2 btn-block">Register</button></a>
-
+            </div>
+            <div class="col-sm-6">
+                <div class="panel-side-img ">
+                    <div class="">
+                        <img src="./assets/Mobile login-rafiki.svg" class="img-fluid " alt="" srcset="">
+                    </div>
                 </div>
             </div>
         </div>
+
+
     </div>
-
-
-</div>
-
-
-
-
-
-
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/bootstrap.js"></script>
 </body>
+
 </html>
--- forest/products.php
+++ forest/products.php
@@ -25,7 +25,7 @@
                 <h1 class="text-center">CHOOSE PRODUCTS</h1>
                 <div class="row pb-5">
                     <div class="col-sm-4 mt-5 shadow">
-                        <input id="myCheck" onclick="myFunction()" name="" id="">
+                        <input id="myCheck" onclick="myFunction()" type="checkbox" name="" id="">
 
                         <div class="p-5">
                             <img src="./assets/Analytics-cuate.svg" class="img-fluid" alt="" srcset="">
@@ -33,7 +33,7 @@
                         <h1 class="text-center text-success">FUEL FIELD</h1>
                     </div>
                     <div class="col-sm-4 mt-5 shadow">
-                        <input id="checked" type="checkbox" name="" id="">
+                        <input id="myCheck" onclick="myFunction()" type="checkbox" name="" id="">
 
                         <div class="p-5">
                             <img src="./assets/Analytics-cuate.svg" class="img-fluid" alt="" srcset="">
@@ -43,14 +43,14 @@
 
                     </div>
                     <div class="col-sm-4 mt-5 shadow">
-                        <input id="myCheck" onclick="myFunction()" name="" id="">
+                        <input type="checkbox" id="myCheck" onclick="myFunction()" name="" id="">
                         <div class="p-5">
                             <img src="./assets/Analytics-cuate.svg" class="img-fluid" alt="" srcset="">
                         </div>
                         <h1 class="text-center text-success">TIMBER</h1>
                     </div>
                     <div class="col-sm-4 mt-5 shadow">
-                        <input id="myCheck" onclick="myFunction()" name="" id="">
+                        <input type="checkbox" id="myCheck" onclick="myFunction()" name="" id="">
 
                         <div class="p-5">
                             <img src="./assets/Analytics-cuate.svg" class="img-fluid" alt="" srcset="">
@@ -69,6 +69,8 @@
 
     </div>
     <script src="./js/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
     <script src="./js/bootstrap.min.js"></script>
     <script src="./js/bootstrap.js"></script>
     <script src="./js/index.js"></script>
@@ -78,17 +80,29 @@
 
 
 <script>
-    function myFunction() {
-        // Get the checkbox
-        var checkBox = document.getElementById("myCheck");
-        // Get the output text
-        var col = document.getElementsByClassName("col-sm-4");
+    // function myFunction() {
+    //     // Get the checkbox
+    //     var checkBox = document.getElementById("myCheck");
+    //     // Get the output text
+    //     var col = document.getElementsByClassName("col-sm-4");
 
-        // If the checkbox is checked, display the output text
-        if (checkBox.checked == true) {
-            text.style.background = "green";
-        } else {
-            text.style.display = "none";
-        }
-    }
+    //     // If the checkbox is checked, display the output text
+    //     if (checkBox.checked == true) {
+    //         checkBox.style.background = "green";
+    //     } else {
+    //         text.style.display = "none";
+    //     }
+    // }
+</script>
+<script>
+    $(document).ready(function() {
+        $("#but").click(function() {
+            if ($("input[type=checkbox]").is(
+                    ":checked")) {
+                alert("Check box in Checked");
+            } else {
+                alert("Check box is Unchecked");
+            }
+        });
+    });
 </script>
