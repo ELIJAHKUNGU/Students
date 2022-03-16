7966991dc2b3de175149915962c41c7f05adb3aa
ELIJAHKUNGU <elijahkungu100@gmail>
2022-03-16 23:34:47 +0300

    quizs

---
+++ forest/quiz.php
@@ -0,0 +1,197 @@
+<?php
+include 'sidebar.php';
+
+
+
+if (isset($_POST["save"])) {
+    require 'db.php';
+    extract($_POST);
+
+    $skidders = $_POST['skidders'];
+    $tractors = $_POST['tractors'];
+    $tractors_no = $_POST['tractors_no'];
+    $chain_saw = $_POST['chain_saw'];
+    $trucks = $_POST['trucks'];
+    // $trucks_hired = $_POST['trucks_hired'];
+    // $trucks_owned = $_POST['trucks_owned'];
+    $trucks_no = $_POST['trucks_no'];
+    $drying_kiln = $_POST['drying_kiln'];
+    $drying_air = $_POST['drying_air'];
+    $debarker = $_POST['debarker'];
+    $hand_saw = $_POST['hand_saw'];
+    $band_saw = $_POST['band_saw'];
+    $amount1 = $_POST['amount1'];
+    
+    // $sql2 = "SELECT * FROM quizs where email = '$user_id'";
+    // $result = mysqli_query($conn,$sql2);
+
+    // if (mysqli_num_rows($result) > 0){
+    //     header("Location:rating.php?error=kindly check rating");
+       
+    // }else{
+        $sql = "INSERT INTO `quizs`( `skidders`, `tractors`, `tractors_no`, `chain_saw`, `trucks`, `trucks1`, `trucks_no`, `drying_kiln`, `drying_air`, `debarker`, `hand_saw`, `band_saw`, `amount1`)
+         VALUES (,'$skidders',$tractors','$tractors_no','$chain_saw','$trucks','$trucks1','$trucks_no','$drying_kiln','$drying_air','$debarker','$hand_saw','$band_saw','$amount1')";
+        $result2 =  mysqli_query($conn, $sql);
+        if ($result2){
+            // $info = mysqli_fetch_assoc($result2);
+
+            // session_start();
+            // $_SESSION["info"] = $info;
+            // header("location:login.php?success=Your account has been  successfully created login  ");
+            header("location:customerdashboard.php?success=Your account has been  successfully created login");
+            exit();
+
+        }else{
+            header("location:quiz.php?error=unknown error occurred&");
+        }
+
+    // }
+
+
+}
+
+?>
+                <style>
+                    .col-sm-6 {
+                        max-width: 500px;
+                        margin-left: 15px;
+                        padding: 20px;
+                        padding-left: 40px;
+                    }
+                    
+                    .row input[type='checkbox'],
+                    .row input[type='number']{
+                   
+                        height: 30px;
+                        width: 30px;
+                        margin-left: 15px;
+                    }
+                    
+                    .row input[type='number'] {
+                        width: 60px;
+                        padding: 5px;
+                    }
+                    .row input[type='radio'] {
+                        height: 20px;
+                        width: 20px;
+                        margin-left: 15px;
+                    }
+                </style>
+                <div class="col-sm-9">
+                    <p>Kindly Answer the above</p>
+                    <hr>
+                    <ol>
+                        <div class="row mt-5">
+                            <div class="col-sm-6 shadow">
+                                <div class="d-flex ">
+                                    <li> <label for="">Skidders</label> </li>
+                                    <input type="checkbox" value="1" class="" name="skidders" id="">
+                                </div>
+                                <div class="d-flex">
+                                    <li> <label for="">Tractors</label> </li>
+                                    <input type="checkbox" value="1" class="mt-2" name="tractors" id="">
+                                </div>
+                                <div class="form-hidden">
+
+                                    <div class="d-flex">
+                                        <li> <label for="">If yes how many</label> </li>
+                                        <input type="number" name="tractors_no" class="">
+
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="d-flex">
+                                        <li> <label for="">Chain Saw</label> </li>
+                                        <input type="checkbox" value="1" name="chain_saw" id="">
+                                    </div>
+                                </div>
+                                <div class="d-flex">
+                                    <li> <label for="">Trucks</label> </li>
+                                    <input type="checkbox" value="1" name="trucks" id="">
+                                </div>
+                                <div class="pl-5">
+                                    <div class="d-flex mt-3">
+                                        <label for="">Hired</label>
+                                        <input name="trucks1" value="1" type="radio">
+                                    </div>
+                                   <div class="form-group">
+                                   <div class="d-flex mt-3">
+                                        <label for="">Owned</label>
+                                        <input name="trucks1" value="1" type="radio">
+                                    </div>
+                                    <div class="d-flex mt-3">
+                                        <label for="">Number</label>
+                                        <input name="trucks1" type="number" min="0">
+                                    </div>
+                                   </div>
+
+                                </div>
+                            </div>
+                            <div class="col-sm-6 shadow">
+                                <div class="form-group">
+                                    <li> <label for="">Drying</label> </li>
+                                    <div class="pl-5">
+                                        <div class="d-flex mt-3">
+                                            <input type="checkbox" value="1" name="drying_kiln" id="">
+                                            <label for="" class="ml-3">Kiln</label>
+
+                                        </div>
+                                        <div class="d-flex mt-3">
+                                            <input type="checkbox" value="1" name="drying_air" id="">
+                                            <label for="" class="ml-3">Air drying</label>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="d-flex">
+                                        <li> <label for="">Debarker</label> </li>
+                                        <input type="checkbox" value="1" name="debarker" id="">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="d-flex">
+                                        <li> <label for="">Hand Saw</label> </li>
+                                        <input name="hand_saw" value="1" type="checkbox">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="d-flex">
+                                        <li> <label for="">Band Saw</label> </li>
+                                        <input name="band_saw" value="1" type="checkbox">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <li> <label for="">Amount handled in the past</label> </li>
+                                    <div class="pl-4">
+                                        <input type="radio" value="1" name="amount1" id="">
+                                        <label for="">Above 1M</label>
+                                    </div>
+                                    <div class="pl-4">
+                                        <input type="radio" value="1" name="amount1" id="">
+                                        <label for="">Above 10M</label>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" name="save" value="Submit" class="form-control bg-success text-white">
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </ol>
+
+                </div>
+            </div>
+        </div>
+
+
+        <script src="./js/jquery.min.js "></script>
+        <script src="./js/bootstrap.min.js "></script>
+        <script src="./js/isotope.js "></script>
+        <script src="./owl-carousel/js/owl.carousel.min.js"></script>
+        <script src="./js/index.js"></script>
+
+    </body>
+
+    </html>
--- forest/sidebar.php
+++ forest/sidebar.php
@@ -1,3 +1,74 @@
+<?php
+ require 'config.php';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/all.min.css" />
+    <link rel="stylesheet" href="./css/bootstrap.css" />
+    <link rel="stylesheet" href="./css/style.css" />
+    <link rel="stylesheet" href="./css/bootstrap.css" />
+    <title>FOREST MANAGEMENT SYSTEM</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+            <a class="navbar-brand" class="nav-item" class="nav-item" href="show.php">FOREST MANAGEMENT SYSTEM</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item ml-auto mr-auto">
+                    <a class="nav-link text-white" href="#">
+                        <button class=" blink btn bg-light    text-dark" style="font-size:13px;" type="button" data-toggle="modal" data-target="#exampleModal" aria-expanded="false" aria-label="Toggle navigation">
+                          Place Order
+                          </button>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">
+                    <span>Welcome
+                        <?php
+                        require 'config.php';
+                        echo   $user_name; 
+
+
+                        ?>
+                    </span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>
+
+            </ul>
+
+        </nav>
+        <?php
+        if(isset($_GET['success'])){?>
+            <div class="alert alert-primary text-center alert-dismissible fade show" role="alert">
+                <?php
+                echo $_GET['success'];
+                ?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <?php
+        }
+        ?>
+    </header>
+
+    <div class="dashboard">
+        <div class="row">
 <div class="col-sm-3 pl-4">
     <div class="mt-3">
         <div class="d-flex">
